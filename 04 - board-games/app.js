747bf5faf40d768f50e64859c1fbdd01745cd6fc
BatByGeek <90051654+BaatyrBeishenaliev@users.noreply.github.com>
2023-02-03 15:13:54 +0600

    Update app.js

--- 04 - board-games/app.js
+++ 04 - board-games/app.js
@@ -1,31 +1,32 @@
-const board = document.querySelector('#board');
-const colors = ['#ff3b3b', '#ff12e7', '#0d7eff', '#eaff00', '#00ff09', '#f08', '#40f'];
+const board = document.querySelector('#board')
+const colors = ['#ff3b3b', '#ff12e7', '#0d7eff', '#eaff00', '#00ff09', '#f08', '#40f']
 const SQUARES_NUMBER = 500;
 
 for (let i = 0; i < SQUARES_NUMBER; i++) {
-    const square = document.createElement('div');
+    const square = document.createElement('div')
     square.classList.add('square');
 
-    square.addEventListener('mouseover', () => setColor(square));
+    square.addEventListener('mouseover', setColor)
 
-    square.addEventListener('mouseleave', () => removeColor(square));
+    square.addEventListener('mouseleave', removeColor)
 
 
-    board.append(square);
+    board.append(square)
 }
 
-function setColor(element) {
+function setColor(event) {
+    const element = event.target
     const color = getRandomColor()
-    element.style.backgroundColor = color;
+    element.style.backgroundColor = color
     element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
 }
 
-function removeColor(element) {
-    element.style.backgroundColor = '#1d1d1d';
+function removeColor(event) {
+    const element = event.target
+    element.style.backgroundColor = '#1d1d1d'
     element.style.boxShadow = `0 0 2px #000`
 }
 
 function getRandomColor() {
-    const index = Math.floor(Math.random() * colors.length);
-    return colors[index];
+    return colors[Math.floor(Math.random() * colors.length)]
 }
